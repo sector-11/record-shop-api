672b1eaba58faba6aa82ab5d5703f76191bfbf5b
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-05 17:12:34 +0100

    feat: implement get on records endpoint to retrieve list of all albums

--- src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
+++ src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
@@ -1,9 +1,30 @@
 package com.northcoders.recordshop.controller;
 
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.service.RecordShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/record-shop")
 public class RecordShopController {
+    @Autowired
+    RecordShopService recordShopService;
+
+    @GetMapping("/records")
+    public ResponseEntity<List<Album>> getAllAlbums(){
+        List<Album> albumList = recordShopService.getAllAlbums();
+
+        if (albumList.isEmpty()){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        } else {
+            return ResponseEntity.ok(albumList);
+        }
+    }
 }
