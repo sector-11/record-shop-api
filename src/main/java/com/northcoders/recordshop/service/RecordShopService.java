940093dfd12ef37ff1cfed9d1b4c3110cb8788d2
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-05 16:05:39 +0100

    feat: implement getallalbums function in service layer

--- src/main/java/com/northcoders/recordshop/service/RecordShopService.java
+++ src/main/java/com/northcoders/recordshop/service/RecordShopService.java
@@ -1,4 +1,9 @@
 package com.northcoders.recordshop.service;
 
+import com.northcoders.recordshop.model.Album;
+
+import java.util.List;
+
 public interface RecordShopService {
+    List<Album> getAllAlbums();
 }
--- src/main/java/com/northcoders/recordshop/service/RecordShopServiceImpl.java
+++ src/main/java/com/northcoders/recordshop/service/RecordShopServiceImpl.java
@@ -1,7 +1,22 @@
 package com.northcoders.recordshop.service;
 
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.repository.RecordShopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RecordShopServiceImpl implements RecordShopService{
+    @Autowired
+    RecordShopRepository recordShopRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albumList = new ArrayList<>();
+        recordShopRepository.findAll().forEach(albumList::add);
+        return albumList;
+    }
 }
