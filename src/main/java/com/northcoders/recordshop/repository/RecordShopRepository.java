01d2721d7ac80dfa8577577f1ed496da5baf2b0a
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-05 11:59:28 +0100

    feat: add recordshoprepository interface

---
+++ src/main/java/com/northcoders/recordshop/repository/RecordShopRepository.java
@@ -0,0 +1,9 @@
+package com.northcoders.recordshop.repository;
+
+import com.northcoders.recordshop.model.Album;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordShopRepository extends CrudRepository<Album, Long> {
+}
