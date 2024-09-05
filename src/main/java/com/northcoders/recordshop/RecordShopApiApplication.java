5fcc31fbc353f72f9534fe0269308957fd89b757
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-05 10:03:37 +0100

    initial project files

---
+++ src/main/java/com/northcoders/recordshop/RecordShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApiApplication.class, args);
+	}
+
+}
