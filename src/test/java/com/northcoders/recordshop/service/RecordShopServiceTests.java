12cf4734b77aba4e31d173e2641996d61f73232b
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-05 16:02:36 +0100

    test: add test for getallalbums in recordshopservice

---
+++ src/test/java/com/northcoders/recordshop/service/RecordShopServiceTests.java
@@ -0,0 +1,45 @@
+package com.northcoders.recordshop.service;
+
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.model.Genre;
+import com.northcoders.recordshop.repository.RecordShopRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class RecordShopServiceTests {
+
+    @Mock
+    RecordShopRepository mockRecordShopRepository;
+
+    @InjectMocks
+    RecordShopServiceImpl recordShopService;
+
+    @Test
+    void TestGetAllAlbums() {
+        //Test data
+        List<Album> albumList = new ArrayList<>();
+        albumList.add(new Album("Black Sabbath", "Black Sabbath", 1970, Genre.METAL));
+        albumList.add(new Album("If You Can Believe Your Eyes and Ears", "The Mamas and the Papas", 1966, Genre.ROCK));
+        albumList.add(new Album("Mm..Food", "MF DOOM", 2004, Genre.HIPHOP));
+        albumList.add(new Album("Voulez-Vous", "ABBA", 1979, Genre.POP));
+
+        //Mock method needed from other layer
+        when(mockRecordShopRepository.findAll()).thenReturn(albumList);
+
+        //Act
+        List<Album> result = recordShopService.getAllAlbums();
+
+        //Assert
+        assertThat(result).hasSize(4);
+        assertThat(result).isEqualTo(albumList);
+    }
+}
