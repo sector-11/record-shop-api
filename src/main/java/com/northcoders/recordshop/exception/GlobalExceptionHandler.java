92d4f9fb50c4f327878e0e4574879263933dc7d5
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-06 12:05:58 +0100

    feat: add globalexceptionhandler class to hold all responses given by exceptions in program for graceful handling of errors

---
+++ src/main/java/com/northcoders/recordshop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.northcoders.recordshop.exception;
+
+import com.northcoders.recordshop.model.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException exception){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionResponse(exception.getMessage(), 500, exception.getClass().getCanonicalName()));
+    }
+}
