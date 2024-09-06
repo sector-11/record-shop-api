bde63783ea61eb3a83ed222dee5fe3f494a35b8f
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-06 14:51:45 +0100

    feat: add handling for case where no id is supplied to /records/ endpoint

--- src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
+++ src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
@@ -1,9 +1,9 @@
 package com.northcoders.recordshop.controller;
 
+import com.northcoders.recordshop.exception.ResourceNotFoundException;
 import com.northcoders.recordshop.model.Album;
 import com.northcoders.recordshop.service.RecordShopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +24,9 @@ public class RecordShopController {
         return ResponseEntity.ok(albumList);
     }
 
-    @GetMapping("records/{id}")
-    public ResponseEntity<Album> getAlbumById(@PathVariable long id){
+    @GetMapping(value = {"/records/{id}", "/records/"})
+    public ResponseEntity<Album> getAlbumById(@PathVariable(required = false, name = "id") Long id){
+        if (id == null) throw new ResourceNotFoundException("No id supplied! You must supply an id to search for on this endpoint!");
         Album album = recordShopService.getAlbumById(id);
         return ResponseEntity.ok(album);
     }
