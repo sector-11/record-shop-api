0f4eddbf6ef9a4f0792ea1e827805bd7c3d16eb6
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-06 18:16:22 +0100

    feat: add aliases to enum values for ease of input with put requests

--- src/main/java/com/northcoders/recordshop/model/Genre.java
+++ src/main/java/com/northcoders/recordshop/model/Genre.java
@@ -1,9 +1,13 @@
 package com.northcoders.recordshop.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public enum Genre {
     POP("Pop"),
     ROCK("Rock"),
+    @JsonAlias({"Hip Hop", "Hip-Hop", "Rap"})
     HIPHOP("Hip-Hop"),
+    @JsonAlias({"RNB", "R&B", "Rhythm and Blues", "R'n'B"})
     RNB("R&B"),
     COUNTRY("Country"),
     JAZZ("Jazz"),
