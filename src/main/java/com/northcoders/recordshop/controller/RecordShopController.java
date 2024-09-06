2b2d237e58f4616b41db6fc04bdf8b112f0d0883
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-06 14:09:29 +0100

    feat: implement /records/id endpoint to get album by id

--- src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
+++ src/main/java/com/northcoders/recordshop/controller/RecordShopController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class RecordShopController {
     public ResponseEntity<List<Album>> getAllAlbums(){
         List<Album> albumList = recordShopService.getAllAlbums();
         return ResponseEntity.ok(albumList);
+    }
+
+    @GetMapping("records/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable long id){
+        Album album = recordShopService.getAlbumById(id);
+        return ResponseEntity.ok(album);
     }
 }
