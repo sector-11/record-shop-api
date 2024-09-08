cb9b65694ac6c51430b5d46d1b8ec42d2ddcfe19
jmc <120180071+sector-11@users.noreply.github.com>
2024-09-08 15:05:43 +0100

    feat: add findbyartist method to recordshoprepository

--- src/main/java/com/northcoders/recordshop/repository/RecordShopRepository.java
+++ src/main/java/com/northcoders/recordshop/repository/RecordShopRepository.java
@@ -4,6 +4,9 @@ import com.northcoders.recordshop.model.Album;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecordShopRepository extends CrudRepository<Album, Long> {
+    List<Album> findByArtist (String artist);
 }
